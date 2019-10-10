fb8f01a9d7e876405dc79ce683bbe00adfe7e261
Max <maxshelepen25@gmail.com>
2019-10-10 20:28:24 +0300

    init: performed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+  let bracket = /\(\)|\{\}|\[\]|\|\||[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/;
+  let cl;
+  const tr = function (str) {
+      const newbrakets = str.replace(bracket, "");
+      if ((newbrakets.length !== 0) && (bracket.test(str))) {
+          tr(newbrakets);
+      } else if (newbrakets.length == 0) {
+          return (cl = true);
+      } else {
+          return (cl = false);
+      }
+  };
+  tr(str);
+  return cl;
 }
